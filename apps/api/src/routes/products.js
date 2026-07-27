066d8cdc448179ad8aa060e78b9b700d8330a397
Mohit Barua <mohit.barua@infobeans.com>
2026-07-27 11:40:06 +0530

    Old code removal

--- apps/api/src/routes/products.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as ctrl from '../controllers/productsController.js';
-
-const router = express.Router();
-
-router.get('/', ctrl.list);
-router.get('/:id', ctrl.getById);
-
-export default router;
